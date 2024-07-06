acf72031b6a4db3baa45f262879cdb5a83a8bf6f
HirenGajjar <gajjarhiren111@gmail.com>
2024-07-05 23:44:51 -0700

    Add http server, socketio and ejs view engine

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const { path } = require("express/lib/application");
 const app = express();
+// Add http server
+const http = require("http");
+const server = http.createServer(app);
+// Add socket io
+const socketio = require("socket.io");
+const io = socketio(server);
+//ejs
 
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+// Routes
 app.get("/", (req, res) => {
   res.send("Hii");
 });
 
-app.listen(3000);
+server.listen(3000);
